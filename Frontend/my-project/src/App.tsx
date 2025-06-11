70d819dfab6f708ef1c3218a2f9ab12e03ae29ec
Pgaikwad203 <priyanshugaikwad30@gmail.com>
2025-06-11 12:21:26 +0530

    Con-4 RegisterForm

--- Frontend/my-project/src/App.tsx
+++ Frontend/my-project/src/App.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import RegisterForm from './Pages/RegisterForm';
-import LoginForm from './Pages/LoginForm';
-import DashboardLayout from './Pages/DashboardLayout';
-
+import React from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import RegisterForm from "./Pages/RegisterForm";
+import LoginForm from "./Pages/LoginForm";
+import DashboardLayout from "./Pages/DashboardLayout";
 
 const App: React.FC = () => (
   <Router>
     <Routes>
       <Route path="/" element={<LoginForm />} />
       <Route path="/register" element={<RegisterForm />} />
-<Route path="/dashboard" element={<DashboardLayout />} />
+      <Route path="/dashboard" element={<DashboardLayout />} />
     </Routes>
   </Router>
 );
